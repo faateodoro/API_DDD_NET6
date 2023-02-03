134706d7f6149e57c2dd55c368d1be12e8c1f3ef
Fabiano Teodoro <faateodoro@gmail.com>
2023-02-03 16:48:13 -0300

    feat: criando o ContextBase

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,41 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Message> Messages { get; set; }
+        DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+        private string GetConnectionString()
+        {
+            return "Driver={SQL Server};Server=localhost;Database=API_DDD_NET6;Uid=terrordoro;Pwd=root;";
+        }
+    }
+}
