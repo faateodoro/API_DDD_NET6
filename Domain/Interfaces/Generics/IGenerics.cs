1678a036f03abc85dac672e96a4c14ae146cc93b
Fabiano Teodoro <faateodoro@gmail.com>
2023-02-03 16:06:45 -0300

    feat: adicionando camada de domain

---
+++ Domain/Interfaces/Generics/IGenerics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGenerics<T> where T : class
+    {
+        Task Add(T objeto);
+        Task Update(T objeto);
+        Task Delete(T objeto);
+        Task<T> GetEntityById(int id);
+        Task<IList<T>> Get();
+    }
+}
